8e480883aa6489f5515865a90b14476e3a3a05d2
Mohsin <rajamohsinali12379@gmail.com>
2023-12-25 20:27:54 +0500

    get attendance of a student for a particular day

--- routes/attendance.js
+++ routes/attendance.js
@@ -4,5 +4,6 @@ const attendanceCtrl = require("../controllers/attendance");
 
 router.post("/mark-attendance", attendanceCtrl.markAttendance);
 router.put("/update-attendance", attendanceCtrl.updateAttendence);
+router.get("/get-attendance-day", attendanceCtrl.getAttendanceOfParticularDay);
 
 module.exports = router;
